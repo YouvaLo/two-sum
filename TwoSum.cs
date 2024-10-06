a011ca94765e16944f053ca966c90c2aea550265
Youva LOUNAS <youva.lounas.97@gmail.com>
2024-10-06 18:08:08 +0200

    Add solution

---
+++ TwoSum.cs
@@ -0,0 +1,33 @@
+using System; 
+
+public class Solution 
+{
+    public static int[] TwoSum(int[] nums, int target) 
+    { 
+        //Declare an empty array
+        int[] indexesTab = {}; 
+
+        //Comparer l'élement i avec tous les élements j  
+        for(int i = 0; i<(nums.Length-1); i++) 
+        {
+            for(int j = i+1; j<(nums.Length); j++)
+            {
+                if(nums[i] + nums[j] == target) indexesTab = [i, j];
+            }           
+        } 
+
+        return indexesTab;
+    }
+
+    public static void Main()
+    {
+        int[] nums = { 1, 2, 3, 6};
+        
+        var indexesTab = TwoSum(nums, 5);
+
+		foreach(var indexTab in indexesTab) 
+		{
+        	Console.Write(indexTab);
+		}
+    }
+}
